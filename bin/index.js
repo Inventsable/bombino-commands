744cbea9baaf767010cc851c810f2bc2bb168379
Tom Scharstein <trscharstein@gmail.com>
2020-02-20 20:02:25 -0700

    Debugging faulty update trigger when using other commands like switch

--- bin/index.js
+++ bin/index.js
@@ -9,9 +9,15 @@ const cmds = {
 };
 
 async function init() {
-  Object.keys(cmds).forEach(action => {
-    if (new RegExp(`${action}`).test(process.argv)) cmds[action]();
-  });
+
+  // Object.keys(cmds).forEach(action => {
+  //   if (new RegExp(`${action}`).test(process.argv)) cmds[action]();
+  // });
+  if (/help/.test(process.argv)) cmds.help()
+  else if (/switch/.test(process.argv)) cmds.switch()
+  else if (/update/.test(process.argv)) cmds.update()
+  else if (/register/.test(process.argv)) cmds.register()
+  else if (/sign/.test(process.argv)) cmds.sign();
 }
 
 init();

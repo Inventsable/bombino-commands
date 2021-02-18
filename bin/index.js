6ae7cbe7a67fb9635f6cabe37942b218741cc18c
Tom Scharstein <trscharstein@gmail.com>
2021-02-18 11:58:24 -0700

    Adding package command as bundle of build, sign, then package

--- bin/index.js
+++ bin/index.js
@@ -6,19 +6,19 @@ const cmds = {
   update: require("../lib/update"),
   register: require("../lib/register"),
   sign: require("../lib/sign"),
+  package: require("../lib/package"),
 };
 
-// What is going on here? Commands are suddenly firing arbitrarily.
-// Update keeps firing on either switch or sign?
 async function init() {
   // Object.keys(cmds).forEach(action => {
   //   if (new RegExp(`${action}`).test(process.argv)) cmds[action]();
   // });
   if (/help/.test(process.argv)) cmds.help();
   else if (/switch/.test(process.argv)) cmds.switch();
+  else if (/update/.test(process.argv)) cmds.update();
   else if (/register/.test(process.argv)) cmds.register();
   else if (/sign/.test(process.argv)) cmds.sign();
-  else if (/update/.test(process.argv)) cmds.update();
+  else if (/package/.test(process.argv)) cmds.package();
 }
 
 init();
